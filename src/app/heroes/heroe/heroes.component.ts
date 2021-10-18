4605759f81cb95ce48ee7b598aa7405504f57a81
Byron Pulgar <byronnv@gmail.com>
2021-10-18 17:42:47 -0300

    Version 1.0.0

---
+++ src/app/heroes/heroe/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'app-heroe',
+    templateUrl : 'heroes.component.html'
+}
+)
+
+export class HeroeComponent{
+    nombre: string = 'Iron Man';
+    edad: number = 45;
+
+    get nombreCapitalizado(): string{
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string{
+        //return ` ${this.nombre} - ${this.edad} `
+        return this.nombre + ' - ' + this.edad.toString();
+    }
+
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad(): void{
+        this.edad = 30;
+    }
+
+
+
+
+}
